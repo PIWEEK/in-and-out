e6a22155ab237e00bce16b6e3f790555ac474cea
Hirunatan <hirunatan@hammo.org>
2019-07-16 16:56:50 +0200

    feat(login): Actual login with popup window
    * feat(login): Actual login with popup window
    
    * fix(login): normalize i18n identifier

--- src/auth/Login.vue
+++ src/auth/Login.vue
@@ -4,9 +4,29 @@
       <h1 class="text-primary-500 font-title text-5xl">
         In & Out
       </h1>
-      <button class="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded">
-        Log in
+      <button
+        class="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded"
+        @click="login()"
+      >
+        {{ $t('login.log-in') }}
       </button>
     </div>
   </div>
 </template>
+
+<script>
+import auth from 'solid-auth-client'
+
+export default {
+  name: 'Login',
+  methods: {
+    async login () {
+      let session = await auth.currentSession()
+      if (!session) {
+        session = await auth.popupLogin({ popupUri: '/login-popup.html' })
+      }
+      console.log('Hay sesión en ', session.webId)
+    },
+  },
+}
+</script>
