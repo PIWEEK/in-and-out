b23dba225aca9283629ae0b6b45b12a889e3d5f1
Andrés Moya <andres.moya@kaleidos.net>
2019-07-16 16:52:44 +0200

    fix(login): normalize i18n identifier

--- src/auth/Login.vue
+++ src/auth/Login.vue
@@ -8,7 +8,7 @@
         class="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded"
         @click="login()"
       >
-        {{ $t('login.Log-in') }}
+        {{ $t('login.log-in') }}
       </button>
     </div>
   </div>
