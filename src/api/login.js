32ded48686e43b15cc045ea37a7f78d5d0596520
Andrés Moya <andres.moya@kaleidos.net>
2019-07-17 14:56:51 +0200

    feat(organizations): Create an organization in a secondary pod

--- src/api/login.js
+++ src/api/login.js
@@ -1,11 +1,7 @@
 import auth from 'solid-auth-client'
+import rdf from 'rdflib'
 
-// Workaround for a bug https://github.com/winstonjs/winston/issues/1354#issuecomment-426433071
-import 'setimmediate'
-
-import ComunicaEngine from 'ldflex-comunica'
-import { PathFactory } from 'ldflex'
-import { namedNode } from '@rdfjs/data-model'
+const VCARD = rdf.Namespace('http://www.w3.org/2006/vcard/ns#')
 
 export async function login () {
   const session = await auth.popupLogin({ popupUri: '/login-popup.html' })
@@ -17,27 +13,50 @@ export async function currentUserWebId () {
   if (!session) {
     return null
   }
-  return session.webId
+  return new rdf.NamedNode(session.webId)
 }
 
 export async function currentUserVCard () {
   const webId = await currentUserWebId()
 
-  const context = {
-    '@context': {
-      '@vocab': 'http://www.w3.org/2006/vcard/ns#',
-    },
-  }
-  const queryEngine = new ComunicaEngine(webId)
-  const path = new PathFactory({ context, queryEngine })
-  const user = path.create({ subject: namedNode(webId) })
-
-  // console.log(`sparql: ${await user.fn.sparql}`)
-  // console.log('path: ', await user.fn.pathExpression)
+  const user = rdf.graph()
+  const fetcher = new rdf.Fetcher(user)
+  await fetcher.load(webId.uri)
 
   return {
-    fullName: await user.fn,
-    organizationName: await user['organization-name'],
-    role: await user.role,
+    fullName: user.any(webId, VCARD('fn')).value,
+    organizationName: user.any(webId, VCARD('organization-name')).value,
+    role: user.any(webId, VCARD('role')).value,
   }
 }
+
+// ***THE SAME With ldflex-comunica***
+//
+// import ComunicaEngine from 'ldflex-comunica'
+// import { PathFactory } from 'ldflex'
+// import { namedNode } from '@rdfjs/data-model'
+//
+// // Workaround for a bug https://github.com/winstonjs/winston/issues/1354#issuecomment-426433071
+// import 'setimmediate'
+
+// export async function currentUserVCard () {
+//   const webId = await currentUserWebId()
+//
+//   const context = {
+//     '@context': {
+//       '@vocab': 'http://www.w3.org/2006/vcard/ns#',
+//     },
+//   }
+//   const queryEngine = new ComunicaEngine(webId)
+//   const path = new PathFactory({ context, queryEngine })
+//   const user = path.create({ subject: namedNode(webId) })
+//
+//   // console.log(`sparql: ${await user.fn.sparql}`)
+//   // console.log('path: ', await user.fn.pathExpression)
+//
+//   return {
+//     fullName: await user.fn,
+//     organizationName: await user['organization-name'],
+//     role: await user.role,
+//   }
+// }
