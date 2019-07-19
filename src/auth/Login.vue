5401fb0949677a347e841fd5daaadb49e1f504f2
Andrés Moya <andres.moya@kaleidos.net>
2019-07-19 09:27:18 +0200

    feat(login): Show the current pod to store data (hardcoded)

--- src/auth/Login.vue
+++ src/auth/Login.vue
@@ -4,6 +4,10 @@
       <h1 class="text-primary font-title text-5xl">
         In & Out
       </h1>
+      <p class="text-secondary text-center">
+        {{ $t('login.current-pod') }}<br>
+        https://pod2.projects.kaleidos.net
+      </p>
       <BaseButton
         kind="primary"
         @click="login()"
