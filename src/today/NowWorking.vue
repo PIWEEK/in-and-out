ccf2c0039dcb7a59f09209fb80c1a547520e4365
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2019-07-19 12:13:36 +0200

    feat(today working): Add some styles

--- src/today/NowWorking.vue
+++ src/today/NowWorking.vue
@@ -1,42 +1,54 @@
 <template>
   <div class="h-full flex flex-col p-8">
     <div class="text-2xl">
-      <div class="flex items-center">
-        <FaIcon icon="play-circle" class="mr-4" />
+      <div class="flex pb-2">
+        <FaIcon icon="play-circle" class="mr-4 text-primary" />
         <div class="flex-1">
+          <div class="text-lg">
+            Hora de entrada
+          </div>
           {{ startRecord.startTime | hourAndMinute }}
         </div>
       </div>
       <div v-if="pauses.length" class="flex pt-2">
-        <FaIcon icon="pause-circle" class="mr-4" />
+        <FaIcon icon="pause-circle" class="mr-4 text-primary" />
         <div class="flex-1">
-          <div v-for="pause in pauses" :key="pause.start.getTime()" class="leading-none pb-2">
+          <div class="text-lg">
+            Pausas
+          </div>
+          <div v-for="pause in pauses" :key="pause.start.getTime()" class="">
             {{ pause.start | hourAndMinute }}
             <template v-if="pause.end">
-              <FaIcon class="mx-2" icon="long-arrow-alt-right" />
-              {{ pause.end | hourAndMinute }}
+              - {{ pause.end | hourAndMinute }}
             </template>
           </div>
         </div>
       </div>
     </div>
-    <div class="flex-1 flex flex-col items-center justify-around text-3xl text-primary">
-      <BaseIconAction
-        v-if="isPaused"
-        icon="resume-circle"
-        :loading="resumeLoading"
-        @click="handleResumeClick"
-      />
-      <BaseIconAction
-        v-else
-        icon="pause-circle"
-        :loading="pauseLoading"
-        @click="handlePauseClick"
-      />
-      <BaseIconAction icon="stop-circle" :loading="stopLoading" @click="handleStopClick" />
+    <div class="flex-1 flex flex-col items-center justify-around text-3xl text-primary py-2">
+      <div v-if="isPaused" class="py-2">
+        <BaseIconAction
+          icon="resume-circle"
+          :loading="resumeLoading"
+          @click="handleResumeClick"
+        />
+      </div>
+      <div v-else class="py-2">
+        <BaseIconAction
+          icon="pause-circle"
+          :loading="pauseLoading"
+          @click="handlePauseClick"
+        />
+      </div>
+      <div class="py-2">
+        <BaseIconAction icon="stop-circle" :loading="stopLoading" @click="handleStopClick" />
+      </div>
     </div>
-    <div class="flex flex-col items-center justify-around text-xl text-primary">
-      TOTAL: {{ runningTotal | duration }}
+    <div class="mt-4 pt-2 text-lg border-t border-gray-600">
+      <span class="ml-12">Total</span>
+      <div class="text-2xl text-center text-primary">
+        {{ runningTotal | duration }}
+      </div>
     </div>
   </div>
 </template>
